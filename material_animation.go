7800795daf96015971079e22157abd9c9e31815a
Unknown <egoavara@gmail.com>
2018-02-02 00:01:05 +0900

    regular upload

--- material_animation.go
+++ material_animation.go
@@ -1,1 +1,16 @@
 package gumi
+
+var Material _MaterialAnimation
+type _MaterialAnimation struct {
+}
+
+func (s _MaterialAnimation) Toggle(t float64) float64 {
+	return Animation.Default(t)
+}
+
+func (s _MaterialAnimation) Button(t float64) float64 {
+	return Animation.Default(t)
+}
+func (s _MaterialAnimation) Progress(t float64) float64 {
+	return Animation.Linear(t)
+}
